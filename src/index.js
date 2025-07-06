4726e00466c3c53596d63b996e314e21795ae96c
Océane Kabasele <72231703+okabasele@users.noreply.github.com>
2025-07-06 04:35:20 +0200

    feat: Add CORS support to the application

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const cors = require("cors")
 const apiRouter = require("./routes")
 const { port } = require("./config/env")
 const db = require("./models")
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
